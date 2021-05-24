8cc304bfb7ce7bd5a1ab537915b68015814f082b
Андрей <62009614+Chigil@users.noreply.github.com>
2021-05-24 09:14:51 +0300

    Update authMiddleware.js

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,7 +5,7 @@ module.exports = function (req,res,next){
         next()
     }
     try{
-        const  token = req.headers.authorization.split('')[1]
+        const  token = req.headers.authorization.split(' ')[1]
         if(!token){
             return res.status(401).json({message:"Не авторизован"})
         }
@@ -15,5 +15,4 @@ module.exports = function (req,res,next){
     } catch (e){
         res.status(401).json({message:"Не авторизован"})
     }
-
-}
+};
